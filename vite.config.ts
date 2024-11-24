aeb206313a433c4d05d8fa770785d4e43e6168ff
jeeyo <jeeyo@outlook.com>
2024-11-24 11:44:20 +0700

    add _headers file

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import generateFile from 'vite-plugin-generate-file'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    // add custom headers to Cloudflare Pages
+    // enabling OPFS feature in the browser
+    generateFile([{
+      type: 'template',
+      output: '_headers',
+      template: './_headerstemplate.ejs',
+      contentType: 'text/plain',
+      data: {
+        paths: {
+          '/*': {
+            'Cross-Origin-Opener-Policy': 'same-origin',
+            'Cross-Origin-Embedded-Policy': 'require-corp',
+          },
+        },
+      },
+    }]),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-  optimizeDeps: {
-    exclude: ['@sqlite.org/sqlite-wasm'],
   },
 })
