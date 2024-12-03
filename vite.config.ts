de3d69da8f0405a9937e69a67b06a18158585326
jeeyo <jeeyo@outlook.com>
2024-12-03 23:27:45 +0700

    add manifest.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import generateFile from 'vite-plugin-generate-file'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-
-    // add custom headers to Cloudflare Pages
-    // enabling OPFS feature in the browser
-    generateFile([{
-      type: 'template',
-      output: '_headers',
-      template: './_headerstemplate.ejs',
-      contentType: 'text/plain',
-      data: {
-        paths: {
-          '/*': {
-            'Cross-Origin-Opener-Policy': 'same-origin',
-            'Cross-Origin-Embedder-Policy': 'require-corp',
-          },
-        },
-      },
-    }]),
   ],
   server: {
     headers: {
