ffc292342f8e558ab8c4cdd905840335263f08fe
Natchanon Nuntanirund <jeeyo@outlook.com>
2024-12-04 12:08:59 +0700

    skip token exist in KV check

--- functions/download/[token].ts
+++ functions/download/[token].ts
@@ -6,12 +6,12 @@ interface Env {
 export const onRequestGet: PagesFunction<Env> = async (context) => {
   const token = Array.isArray(context.params.token) ? context.params.token[0] : context.params.token
 
-  const kv = await context.env.SYNC_TOKEN.get(token);
-  if (kv === null) {
-    return new Response(null, {
-      status: 404,
-    })
-  }
+  // const kv = await context.env.SYNC_TOKEN.get(token);
+  // if (kv === null) {
+  //   return new Response(null, {
+  //     status: 404,
+  //   })
+  // }
 
   const obj = await context.env.SYNC_BUCKET.get(token)
   if (!obj) {
